e01d5d96e8edb7c96d57ae87760662b43e94f8c5
JCMRS <jcmeijers@gmail.com>
2025-10-24 18:49:44 +0200

    feat: Integrate project analyze command into forge.ts

--- forge.ts
+++ forge.ts
@@ -1,7 +1,16 @@
 // forge.ts
 
 import { parseArgs } from "./src/modules/command-parser/main.ts";
+import { analyzeProjectStructure } from "./src/modules/analysis-engine/main.ts";
 
 const parsedCommand = parseArgs(Deno.args);
 
-console.log(`Command: '${parsedCommand.command}', Subcommand: '${parsedCommand.subcommand}', Args: [${parsedCommand.args.map(arg => `'${arg}'`).join(', ')}]`);
+if (parsedCommand.command === "project" && parsedCommand.subcommand === "analyze") {
+  analyzeProjectStructure().then(report => {
+    console.log(report);
+  }).catch(error => {
+    console.error(`Error during project analysis: ${(error as Error).message}`);
+  });
+} else {
+  console.log(`Command: '${parsedCommand.command}', Subcommand: '${parsedCommand.subcommand}', Args: [${parsedCommand.args.map(arg => `'${arg}'`).join(', ')}]`);
+}
